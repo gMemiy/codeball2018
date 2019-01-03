276a11f6653bd9e33541f373bec69b22a0a4b734
gMemiy <am1369a@gmail.com>
2019-01-03 21:55:03 +0300

    init

---
+++ MyStrategy.java
@@ -0,0 +1,12 @@
+import model.*;
+
+public final class MyStrategy implements Strategy {
+    @Override
+    public void act(Robot me, Rules rules, Game game, Action action) {
+    }
+    
+    @Override
+    public String customRendering() {
+        return "";
+    }
+}
