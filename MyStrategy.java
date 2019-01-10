b5c056a23a5fc10b1e3f4bc43ed5e6692b8b365c
gMemiy <am1369a@gmail.com>
2019-01-11 01:03:40 +0300

    jumping

--- MyStrategy.java
+++ MyStrategy.java
@@ -15,13 +15,7 @@ public final class MyStrategy implements Strategy {
             action.use_nitro = true;
             return;
         }
-        
-        boolean jump = DistBetweenPoint(new Point(ball), new Point(me)) <
-                (rules.BALL_RADIUS + rules.ROBOT_MAX_RADIUS) &&
-                (ball.y - ball.radius) <= (rules.ROBOT_MAX_RADIUS*4.0) &&
-                me.y < ball.y && 
-                me.z < ball.z;
-        
+               
         boolean is_attacker = true;
         for (Robot robot : game.robots)
         {
@@ -35,7 +29,7 @@ public final class MyStrategy implements Strategy {
             }
         }
         
-        if ((is_attacker || ball.z < (-rules.arena.depth / 5)) && 
+        if (is_attacker &&
                 ball.z > me.z)
         {
             for (int i = 0; i < 100; ++i)
@@ -44,91 +38,111 @@ public final class MyStrategy implements Strategy {
                 Ball ball_pos = CalcNewBallPos(ball, t, rules);
                 for (Robot r : game.robots)
                 {
-                    CollideRobots(ball_pos, r, rules);
+                    if (r.id != me.id)
+                    {
+                        CollideRobots(ball_pos, r, rules);
+                    }
                 }
-                // Если мяч не вылетит за пределы арены
-                // (произойдет столкновение со стеной, которое мы не рассматриваем),
-                // и при этом мяч будет находится ближе к вражеским воротам, чем робот,
+
                 if (is_attacker)
                 {
-                    ball_pos.x += 2*ball.radius*(ball.x/(rules.arena.width/2.0));
+                    ball_pos.x += ball.radius*(ball_pos.x/(rules.arena.width/2.0)) + ball.radius*(ball_pos.z/(rules.arena.depth/2.0));
                 }
                 if (ball_pos.z > me.z)
-                    //&& Math.abs(ball.x) < (rules.arena.width / 2.0)
-                    //&& Math.abs(ball.z) < (rules.arena.depth / 2.0))
                 {                   
-                    // Посчитаем, с какой скоростью робот должен бежать,
-                    // Чтобы прийти туда же, где будет мяч, в то же самое время
                     double delta_pos_x = ball_pos.x - me.x;
-                    double delta_pos_z = ball_pos.z - me.z;
-                    double delta_pos_dist = Math.sqrt(delta_pos_x*delta_pos_x + delta_pos_z*delta_pos_z);
+                    double delta_pos_y = ball_pos.y + ball.radius/2 - me.y;
                     
-                    if (is_attacker && ball.z < (-rules.arena.depth / 4.0))
+                    double addZ = 0;
+                    if (ball.velocity_y > EPS && delta_pos_y > rules.ROBOT_MAX_JUMP_SPEED*t)
                     {
-                        Point nearest = NearestEnemy(new Point(me), game.robots);
-                        if (nearest != null)
-                        {
-                            double distToEnemy = DistBetweenPoint(new Point(me), nearest);
-                            delta_pos_dist = distToEnemy;
-                            delta_pos_x = nearest.x - me.x;
-                            delta_pos_z = nearest.z - me.z;
-                        }
+                        //addZ = -delta_pos_y/2;
                     }
+                    double delta_pos_z = ball_pos.z - me.z + addZ;
+                    
+                    double delta_pos_dist = Math.sqrt(
+                            Math.sqrt(delta_pos_x*delta_pos_x + delta_pos_z*delta_pos_z)*
+                            Math.sqrt(delta_pos_x*delta_pos_x + delta_pos_z*delta_pos_z)+
+                            delta_pos_y*delta_pos_y
+                    );
+                                       
+                    double jump_speed = delta_pos_y / t;
+                    double target_jump_speed = 0;
+                    if (0.5*rules.ROBOT_MAX_JUMP_SPEED <= jump_speed &&
+                            jump_speed <= rules.ROBOT_MAX_JUMP_SPEED &&
+                            delta_pos_dist <= delta_pos_y*2)//rules.ROBOT_MAX_JUMP_SPEED*t)
+                    {
+                        target_jump_speed = jump_speed;
+                    }
+                    
                     double need_speed = delta_pos_dist / t;
                     // Если эта скорость лежит в допустимом отрезке
                     if (0.5 * rules.ROBOT_MAX_GROUND_SPEED < need_speed
                         && need_speed < rules.ROBOT_MAX_GROUND_SPEED)
                     {
+                        Point v = new Point(delta_pos_x / delta_pos_dist, 0, delta_pos_z / delta_pos_dist);
+                        if (target_jump_speed > 0)
+                        {
+                            v = Mult(v, rules.ROBOT_MAX_GROUND_SPEED);
+                        }
+                        else
+                        {
+                            v = Mult(v, need_speed);
+                        }
                         // То это и будет наше текущее действие
-                        action.target_velocity_x = delta_pos_x / delta_pos_dist * need_speed;
-                        action.target_velocity_z = delta_pos_z / delta_pos_dist * need_speed;
+                        action.target_velocity_x = v.x;//delta_pos_x / delta_pos_dist * need_speed;
+                        action.target_velocity_z = v.z;//delta_pos_z / delta_pos_dist * need_speed;
                         action.target_velocity_y = 0.0;
-                        action.jump_speed = jump ? rules.ROBOT_MAX_JUMP_SPEED : 0.0;
+                        action.jump_speed = target_jump_speed;
                         action.use_nitro = false;
                         return;
                     }
                 }
             }
         }
-        // Стратегия защитника (или атакующего, не нашедшего хорошего момента для удара):
-        // Будем стоять посередине наших ворот
-        double target_pos_x = ball.x;
-        
-        if (target_pos_x > rules.arena.goal_width / 2)
-        {
-            target_pos_x = rules.arena.goal_width / 2;
-        }
 
-        if (target_pos_x < -rules.arena.goal_width / 2)
-        {
-            target_pos_x =  -rules.arena.goal_width / 2;
-        }
+        double target_pos_x = (ball.x / (rules.arena.width / 2))*(rules.arena.goal_width / 2 + ball.radius);
         
-        double target_pos_z = -(rules.arena.depth / 2.0) + rules.arena.bottom_radius - rules.ROBOT_MIN_RADIUS;
+        double target_pos_z = -(rules.arena.depth / 2.0) - rules.arena.bottom_radius;
         if (is_attacker)
         {
-            target_pos_z = -rules.arena.depth / 4.0;
+            target_pos_z = -rules.arena.depth/3;
         }
+        double target_jump_speed = 0; 
         // Причем, если мяч движется в сторону наших ворот
         if (ball.velocity_z < -EPS) {
             // Найдем время и место, в котором мяч пересечет линию ворот
-            double t = (target_pos_z - ball.z) / ball.velocity_z;
+            target_pos_z = -rules.arena.depth / 2.0;
+            double t = target_pos_z / ball.velocity_z;
             double x = ball.x + ball.velocity_x * t;
+            double y = ball.y + ball.velocity_y * t;
             // Если это место - внутри ворот
             if (Math.abs(x) < (rules.arena.goal_width / 2.0)) {
                 // То пойдем защищать его
                 target_pos_x = x;
+               
+                double jump_speed = (y - me.y) / t;
+                if (jump_speed >= 0.5*rules.ROBOT_MAX_JUMP_SPEED &&
+                    jump_speed <= rules.ROBOT_MAX_JUMP_SPEED)
+                {
+                    target_jump_speed = jump_speed;
+                }
             }
         }
-
+        
         // Установка нужных полей для желаемого действия
         double target_velocity_x = (target_pos_x - me.x)*rules.ROBOT_MAX_GROUND_SPEED;
-        double target_velocity_z = (target_pos_z - me.z)*rules.ROBOT_MAX_GROUND_SPEED;
-
+        double target_velocity_z = (target_pos_z - me.z)*rules.ROBOT_MAX_GROUND_SPEED;        
+        
+        if (target_jump_speed > 0)
+        {
+            target_velocity_z = rules.ROBOT_MAX_GROUND_SPEED;
+        }
+        
         action.target_velocity_x = target_velocity_x;
         action.target_velocity_z = target_velocity_z;
         action.target_velocity_y = 0.0;
-        action.jump_speed = jump ? rules.ROBOT_MAX_JUMP_SPEED : 0.0;
+        action.jump_speed = target_jump_speed;
         action.use_nitro = false;
     }
     
